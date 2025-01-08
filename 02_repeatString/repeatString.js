7fdd4700cc4526d54b925a4bd8211d5a442fb123
mohamedx97 <mohamedmailone@gmail.com>
2025-01-08 22:11:00 +0200

    solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(word,countOfrepeateing) {
+    repeatedText = ""
+    if (countOfrepeateing < 0)
+    {
+        return "ERROR";
+    }
+    else
+    {
+        for (i = countOfrepeateing;i > 0;i--)
+        {
+            repeatedText = repeatedText + word
+        }
+        return repeatedText
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
