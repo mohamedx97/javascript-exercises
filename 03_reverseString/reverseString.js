d57e59e5b82ce348e3ca5fa48285f0e50b5e6484
mohamedx97 <mohamedmailone@gmail.com>
2025-01-08 23:05:31 +0200

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(textToReverse) {
+    reversedArray = [];
+    for (i = textToReverse.length; i > 0; i--){
+        reversedArray.push(textToReverse[i-1]);
+    }
+    result = reversedArray.join("");
+    return result;
 };
 
 // Do not edit below this line
