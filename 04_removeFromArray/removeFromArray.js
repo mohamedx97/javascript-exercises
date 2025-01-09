eaeed558f08cea6926657e81d6bb118e3acc48d8
mohamedx97 <mohamedmailone@gmail.com>
2025-01-09 23:10:14 +0200

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayToRemoveFrom, ...args) {
+  for (argument of args) {
+    while (arrayToRemoveFrom.includes(argument)){
+        for (arg of arrayToRemoveFrom){
+            if (arg === argument){
+                arrayToRemoveFrom.splice(arrayToRemoveFrom.indexOf(arg), 1);
+            }
+        }
+    }
+  }
+  return arrayToRemoveFrom;
 };
 
 // Do not edit below this line
