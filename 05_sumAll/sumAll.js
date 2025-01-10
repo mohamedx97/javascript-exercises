0c0adf674f1199e4961c4b5e80139f3312fc2b60
mohamedx97 <mohamedmailone@gmail.com>
2025-01-10 20:45:42 +0200

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }else if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    
+    let theBigNum = 0;
+    let theSmallNum = 0;
+    let seriesLen = 0;
+   
+    if (num1 > num2){
+        theBigNum = num1;
+        theSmallNum = num2;
+        seriesLen = theBigNum - theSmallNum + 1;
+        return (((theBigNum + theSmallNum))*seriesLen) / 2;
+    }
+    else if(num2 > num1){
+        theBigNum = num2;
+        theSmallNum = num1;
+        seriesLen = theBigNum - theSmallNum + 1;
+        return (((theBigNum + theSmallNum))*seriesLen) / 2;
+    }
+    else{
+        return 0;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
